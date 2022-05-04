8d3ac54e1e6985f4c729571c566c6f4c704976df
Admin <mesore@mail.ru>
2022-05-04 17:42:33 +0300

    добавлен счастливый билет

---
+++ 1_12 Счастливый билет.py
@@ -0,0 +1,8 @@
+number = input()
+num1 = int(number[0]) + int(number[1]) + int(number[2])
+num2 = int(number[3]) + int(number[4]) + int(number[5])
+
+if num1 == num2:
+    print('Счастливый')
+else:
+    print('Обычный')
