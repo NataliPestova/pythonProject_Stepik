3558cf60859f70beb5d84f99522aa93a9fdaa1c3
Admin <mesore@mail.ru>
2022-05-24 11:16:44 +0300

    2_2 задача с continue

--- Programmirovanie na Python/2/2_2 break continue.py
+++ Programmirovanie na Python/2/2_2 break continue.py
@@ -1,10 +1,8 @@
 a = int(input())
-while    a <= 100:
+while a <= 100:
     if a < 10:
         a = int(input())
         continue
-    elif a > 100:
-        break
     else:
         print(a)
         a = int(input())
